5be6e61a8e62745cfb136e69aafbde2ef140f228
wxy@jlwh.com <wxy@jlwh.com>
2022-09-21 14:20:31 +0800

    first commit

--- src/config/index.js
+++ src/config/index.js
@@ -1,10 +1,6 @@
 
 export const API_HOST = process.env.VUE_APP_API_HOST || window.location.protocol+"//"+window.location.host+"/api"
 
-export const ERC20_FULL_HOST = process.env.VUE_APP_ERC20_FULL_HOST
-export const TRC20_FULL_HOST = process.env.VUE_APP_TRC20_FULL_HOST
-export const BEP20_FULL_HOST = process.env.VUE_APP_BEP20_FULL_HOST
-
 export const ROUTER_WHITELIST = [
     "/401", "/404", "/500","/readme/main"
 ]
